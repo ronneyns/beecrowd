35a9efe7603113e9743440ba6c4e485a751c53ae
Ronney N.S. <ronneyns@gmail.com>
2024-11-17 13:28:06 -0300

    1028

--- 5 - Matemática/1028.py
+++ 5 - Matemática/1028.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 
+# ------------------------------------------------------------------------------
+def mdc_divisoes_sucessivas(x: int, y: int) -> int:
+	while y > 1:
+		x, y = y, x % y
+	return x
+
+# ------------------------------------------------------------------------------
 for _ in range(int(input())):
 	f1, f2 = [int(x) for x in input().strip().split()]
 
 	# Se f2 > f1, troque os valores
 	if f2 > f1:
-		aux = f1
-		f1 = f2
-		f2 = aux
-
-	# MDC por Divisões Sucessivas
-	while f2 > 1:
-		aux = f1 % f2;
-		f1 = f2;
-		f2 = aux;
-	print(f1)
+		f1, f2 = f2, f1
+	print(mdc_divisoes_sucessivas(f1, f2))
 
