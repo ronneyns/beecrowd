f09d607db97e5d9b68be4e28f36381a7377518d8
Ronney N.S. <ronneyns@gmail.com>
2024-11-16 22:21:27 -0300

    1025

--- 4 - Estruturas e Bibliotecas/1025.py
+++ 4 - Estruturas e Bibliotecas/1025.py
@@ -1,22 +1,30 @@
-n, q = [int(x) for x in input().strip().split()]
+from bisect import bisect_left # https://docs.python.org/3/library/bisect.html
+
+# ------------------------------------------------------------------------------
+def BinarySearch(lista: list, valor: int) -> int:
+	pos = bisect_left(lista, valor)
+	if (pos != len(lista)) and (lista[pos] == valor):
+		return pos
+	return -1
+
+# ------------------------------------------------------------------------------
 caso = 1
 
+n, q = [int(x) for x in input().strip().split()]
 while (n != 0) and (q != 0):
 	marmores = []
 	for i in range(n):
 		marmores.append(int(input()))
 	marmores.sort()
 
-	consultas = []
-	for i in range(q):
-		consultas.append(int(input()))
-
 	print(f'CASE# {caso}:')
-	for x in consultas:
-		if x in marmores:
-			print(f'{x} found at {marmores.index(x) + 1}')
+	for i in range(q):
+		valor = int(input())
+		pos = BinarySearch(marmores, valor)
+		if pos == -1:
+			print(f'{valor} not found')
 		else:
-			print(f'{x} not found')
+			print(f'{valor} found at {pos + 1}')
 	n, q = [int(x) for x in input().strip().split()]
 	caso += 1
 
