fe17110c25d0e3f86a3c01490632ad2f527ae0c3
Ronney N.S. <ronneyns@gmail.com>
2024-11-17 03:02:52 -0300

    1024

---
+++ 3 - Strings/1024.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+for _ in range(int(input())):
+    mensagem = input()
+    saida = [chr(ord(c) + 3) if c.isalpha() else c for c in mensagem]
+    saida.reverse()
+    meio = len(saida) // 2
+    saida = saida[:meio] + [chr(ord(c) - 1) for c in saida[meio:]]
+    print(''.join(saida))
+
