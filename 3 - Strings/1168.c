ae6c9289a6f1947c7b2d7b53a3ab73d65642ecce
Ronney N.S. <ronneyns@gmail.com>
2024-11-25 19:53:33 -0300

    1168

--- 3 - Strings/1168.c
+++ 3 - Strings/1168.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-int conta_leds(char *valor, int tamanho) {
+int contar_leds(char *valor, int tamanho) {
 	int qtde_leds[] = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6};
 	int qtde;
 
 	qtde = 0;
 	for (int i = 0; i < tamanho; i++) {
-		qtde += qtde_leds[valor[i] - 0x30];
+		qtde += qtde_leds[valor[i] - '0'];
 	}
 	return qtde;
 }
@@ -19,8 +19,9 @@ int main() {
 	scanf("%d", &n);
 	for (; n > 0; n--) {
 		scanf("%s", valor);
-		printf("%d leds\n", conta_leds(valor, strlen(valor)));
+		printf("%d leds\n", contar_leds(valor, strlen(valor)));
 	}
  
     return 0;
 }
+
