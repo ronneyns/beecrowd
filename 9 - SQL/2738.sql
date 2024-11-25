dc98d585b3ef5d05842a50ee95e04c2ce2478cb2
Ronney N.S. <ronneyns@gmail.com>
2024-11-24 21:53:17 -0300

    2738

--- 9 - SQL/2738.sql
+++ 9 - SQL/2738.sql
@@ -3,7 +3,7 @@ SELECT
   ROUND((sco.math * 2 + sco.specific * 3 + sco.project_plan * 5) / 10, 2) AS media
 FROM
   candidate AS can
-  INNER JOIN score as sco ON
+  INNER JOIN score AS sco ON
     sco.candidate_id = can.id
 ORDER BY
   2 DESC;
