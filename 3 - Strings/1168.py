e6f4516559fb976b4b93703d6ab8f5cd29b172ba
Ronney N.S. <ronneyns@gmail.com>
2024-11-28 22:47:18 -0300

    1168

---
+++ 3 - Strings/1168.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+def contar_leds(valor: str) -> int:
+	qtde_leds = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6]
+	return sum([qtde_leds[ord(c) - ord('0')] for c in valor])
+
+for _ in range(int(input())):
+	valor = input()
+	print(f'{contar_leds(valor)} leds');
+
