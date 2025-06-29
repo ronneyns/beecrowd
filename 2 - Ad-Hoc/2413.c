3c019b04bd5f362fcc6d9af9e4386b09d9b35bfe
Ronney N.S. <ronneyns@gmail.com>
2025-06-28 22:08:00 -0300

    2413

---
+++ 2 - Ad-Hoc/2413.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int qtde_pessoas_3o_link;
+
+	while (scanf("%d", &qtde_pessoas_3o_link) != EOF) {
+		printf("%d\n", qtde_pessoas_3o_link * 4);
+	}
+	return 0;
+}
+
--- 2 - Ad-Hoc/README.md
+++ 2 - Ad-Hoc/README.md
@@ -4,4 +4,5 @@ Problemas de Simulação, Datas e Ad-Hoc no geral
 ## Problemas Resolvidos
 - 1026: Carrega ou não Carrega?
 - 1030: A Lenda de Flavious Josephus
+- 2413: Busca na Internet
 
