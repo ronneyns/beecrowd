12bc04a53e1fb7895730dfec248569409e47b0c6
Ronney N.S. <ronneyns@gmail.com>
2025-07-05 13:49:37 -0300

    1219

--- 5 - Matemática/1219.c
+++ 5 - Matemática/1219.c
@@ -10,19 +10,24 @@ int main() {
 	double raio_circ_pequeno, area_circ_pequeno;
 
 	while (scanf("%d%d%d", &a, &b, &c) != EOF) {
+		// Semiperímetro do trângulo
 		semiperimetro = (double) (a + b + c) / 2;
 
+		// Área do triângulo (Fórmula de Heron)
 		// https://www.youtube.com/watch?v=kXyF_fdQEd0
 		area_triangulo = sqrt(semiperimetro * (semiperimetro - a) * (semiperimetro - b) * (semiperimetro - c));
 
+		// Área do triângulo pelo círculo inscrito
 		// https://www.youtube.com/watch?v=OV0nydAi7fw
 		raio_circ_pequeno = area_triangulo / semiperimetro;
 		area_circ_pequeno = PI * pow(raio_circ_pequeno, 2);
 
+		// Área do triângulo inscrito numa circunferência
 		// https://www.youtube.com/watch?v=8Nyy1b9qu00
 		raio_circ_grande = (a * b * c) / (4 * area_triangulo);
 		area_circ_grande = PI * pow(raio_circ_grande, 2);
 
+		// Ajuste nas áreas "externas" em função das "internas"
 		area_circ_grande -= area_triangulo;
 		area_triangulo -= area_circ_pequeno;
 		printf("%.4f %.4f %.4f\n", area_circ_grande, area_triangulo, area_circ_pequeno);
